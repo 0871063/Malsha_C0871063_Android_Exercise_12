78c17da5c4222cf5bddf908daa00461ec45fa124
0871063 <c0871063@mylambton.ca>
2022-11-17 14:05:17 -0500

    Exercise One Complete

---
+++ src/Exercise_1.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercise_1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert array count : ");
+        int arrayCount = scanner.nextInt();
+
+        int[] numberArray = new int[arrayCount];
+
+        for (int i = 0 ; i< numberArray.length; i++){
+            System.out.println("Please insert a number : ");
+            numberArray[i] = scanner.nextInt();
+        }
+        if (numberArray.length == 0){
+            System.out.println("Table is empty.\n");
+        }else {
+
+            int count = 1, tempCount;
+            int number = numberArray[0];
+            int temp = 0;
+
+            for (int i = 0; i < (numberArray.length - 1); i++)
+            {
+                temp = numberArray[i];
+                tempCount = 0;
+                for (int j = 1; j < numberArray.length; j++)
+                {
+                    if (temp == numberArray[j])
+                        tempCount++;
+                }
+                if (tempCount > count)
+                {
+                    number = temp;
+                    count = tempCount;
+                }
+            }
+
+            System.out.println("The most frequent number in the table is:");
+            System.out.printf("%d ( %d x )" , number, count);
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+    static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    static final int SHIFT = 4;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Please insert a string of characters : ");
+        String s = scanner.nextLine();
+        // the string to encode
+        String toEncode = "";
+        // the coded string
+        String chainCode = "";
+/******************************************* *******************************************/
+// write your program in this between
+/******************************************* *******************************************/
+        System.out.format("the initial chain was : '%s'\n", s);
+        if (toEncode.isEmpty()) {
+            System.out.println("the string to encode is empty.\n");
+        } else {
+            System.out.format("the string to encode is : '%s'\n", toEncode);
+            System.out.format("the coded string is : '%s'\n", chainCode);
+        }
     }
 }
